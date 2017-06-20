06fe01ddb90115c7082e15f1d74faf4551bbd706
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-20 17:23:21 -0400

    ahh

--- pages/db.php
+++ pages/db.php
@@ -4,7 +4,7 @@ $user= "root";
 $pass= "password";
 $db= "alyan";
 
-$mysqli= new mysqli('alyandb.cmydnnixocdw.us-west-2.rds.amazonaws.com:3306','root','password', "alyan")or die($mysqli->error);
+$mysqli= new mysqli($host,'root','password', "alyan")or die($mysqli->error);
 echo mysqli_ping() ;
 if (!$mysqli) {
     die("Connection failed: " . mysqli_connect_error());
