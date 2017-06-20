16eaf8d2e90edb9faaed143c0932d3bc6ee2cf4f
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-20 17:32:26 -0400

    ahh

--- pages/db.php
+++ pages/db.php
@@ -1,9 +1,14 @@
 <?php
-$host= "alyandb.cmydnnixocdw.us-west-2.rds.amazonaws.com";
+$host= "alyandb.cmydnnixocdw.us-west-2.rds.amazonaws.com:";
 $user= "root";
 $pass= "password";
 $db= "alyan";
 
+if (!function_exists('mysqli_init') && !extension_loaded('mysqli')) {
+    echo 'We don\'t have mysqli!!!';
+} else {
+    echo 'Phew we have it!';
+}
 $mysqli= new mysqli($host,'root','password', "alyan")or die($mysqli->error);
 echo mysqli_ping() ;
 if (!$mysqli) {
