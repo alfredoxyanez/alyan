c2e4a79d440c50a851aa5049e1d55e11be5e41dc
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-20 17:13:52 -0400

    ahh

--- pages/db.php
+++ pages/db.php
@@ -5,8 +5,7 @@ $pass= "password";
 $db= "alyan";
 
 $mysqli= new mysqli('alyandb.cmydnnixocdw.us-west-2.rds.amazonaws.com:3306','root','password', "alyan")or die($mysqli->error);
-$message = "youdid not fucked up";
-echo "<script type='text/javascript'>alert('$message');</script>";
+echo mysqli_ping() ;
 if (!$mysqli) {
     $message = "you fucked up";
     echo "<script type='text/javascript'>alert('$message');</script>";
