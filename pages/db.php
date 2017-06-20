7850736d3a41fc8bb3bf87e6c4925555983236bf
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-20 17:11:04 -0400

    ahh

--- pages/db.php
+++ pages/db.php
@@ -7,6 +7,8 @@ $db= "alyan";
 $mysqli= new mysqli('alyandb.cmydnnixocdw.us-west-2.rds.amazonaws.com:3306','root','password', "alyan")or die($mysqli->error);
 
 if (!$mysqli) {
+    $message = "you fucked up";
+    echo "<script type='text/javascript'>alert('$message');</script>";
     die("Connection failed: " . mysqli_connect_error());
 }
  ?>
