37d0af39b5120628b7e4d1cc622011f59786a008
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-21 14:02:02 -0400

    generic

--- pages/db.php
+++ pages/db.php
@@ -4,9 +4,8 @@ $user= "root";
 $pass= "password";
 $db= "alyan";
 
-$mysqli= mysqli_connect("alyanpr.cmydnnixocdw.us-west-2.rds.amazonaws.com:3306",'alyanadmin','password', "innodb")or die($mysqli->error);
-$mysqli=mysqli_connect("alyanpr.cmydnnixocdw.us-west-2.rds.amazonaws.com",'alyanadmin','password',
-"innodb",'3306')or die($mysqli->error);
+//$mysqli= mysqli_connect("alyanpr.cmydnnixocdw.us-west-2.rds.amazonaws.com:3306",'alyanadmin','password', "innodb")or die($mysqli->error);
+$mysqli=mysqli_connect("alyanpr.cmydnnixocdw.us-west-2.rds.amazonaws.com",'alyanadmin','password','innodb','3306')or die($mysqli->error);
 // $message= mysqli_ping($mysqli) ;
 // echo "<script type='text/javascript'>alert('$message');</script>";
 // if (!$mysqli) {
