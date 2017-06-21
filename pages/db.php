7ee271444de22eb2471ae00ffdd3ef09f9a09377
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-21 03:10:08 -0400

    ahh

--- pages/db.php
+++ pages/db.php
@@ -5,9 +5,9 @@ $pass= "password";
 $db= "alyan";
 
 $mysqli= mysqli_connect("alyanpr.cmydnnixocdw.us-west-2.rds.amazonaws.com:3306",'alyanadmin','password', "innodb")or die($mysqli->error);
-$message= mysqli_ping($mysqli) ;
-echo "<script type='text/javascript'>alert('$message');</script>";
-if (!$mysqli) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+// $message= mysqli_ping($mysqli) ;
+// echo "<script type='text/javascript'>alert('$message');</script>";
+// if (!$mysqli) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
  ?>
