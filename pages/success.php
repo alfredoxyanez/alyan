9cf0c47ea9bddd82714652b98e2ac840650833eb
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-23 14:39:18 -0400

    generic

--- pages/success.php
+++ pages/success.php
@@ -46,7 +46,7 @@
           <div class="panel-body">
             <p>
               <?php
-              if( isset($_SESSION['message'] AND !empty($_SESSION('message')))){
+              if( isset($_SESSION['message']) AND !empty($_SESSION('message'))){
                 echo $_SESSION['message']
               }else{
                 header("location: index.php")
