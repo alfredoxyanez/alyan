fec0b1dc038da4e322b64d50e18381429053e67f
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-26 12:23:38 -0400

    generic

--- pages/addvalve.php
+++ pages/addvalve.php
@@ -6,8 +6,8 @@ require 'json.php';
 $parkname = $mysqli->escape_string($_POST['parkname']);
 $parkname= getname($parkname);
 $valveid = intval($mysqli->escape_string($_POST['valveid']));
-echo $parkname;
-echo $valveid;
+// echo $parkname;
+// echo $valveid;
 addvalvej($valveid,$parkname);
 
 
