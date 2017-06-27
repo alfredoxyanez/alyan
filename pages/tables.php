8b1b576f9a1067543a9008a5c48174152fb4dbbd
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-27 16:12:14 -0400

    generic

--- pages/tables.php
+++ pages/tables.php
@@ -158,7 +158,7 @@ function logout(){
       <div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav navbar-collapse">
           <ul class="nav" id="side-menu">
-            <h3>Hello, <?php echo loginfname() ?> !</h3>
+            <h3 class="text-center">Hello, <?php echo loginfname() ?> !</h3>
 
             <li>
               <a href="tables.php"><i class="fa fa-home fa-fw"></i> All Parks</a>
