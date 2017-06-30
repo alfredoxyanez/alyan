806446057a6f5231d21f58b1e937ac2a0cc7adfc
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-29 20:52:40 -0400

    generic

--- pages/parkpage.php
+++ pages/parkpage.php
@@ -530,7 +530,7 @@ function logout(){
       latlng= latlng.split("?");
       var uluru = {lat: -25.363, lng: 131.044};
       var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 7,
         center: uluru
       });
       var marker = new google.maps.Marker({
