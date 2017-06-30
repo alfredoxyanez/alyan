b0a003d490334e84bdd2c821a569dd72a8ea4f2c
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-06-29 21:37:08 -0400

    generic

--- pages/parkpage.php
+++ pages/parkpage.php
@@ -267,7 +267,7 @@ function logout(){
 
   <style>
   #map {
-    height: 300px;
+    height: 400px;
     width: 100%;
   }
 
@@ -556,6 +556,7 @@ function initMap() {
   });
 
 }
+window.onload = loadScript;
 </script>
 <script async defer src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD6RsQuot1EGNW89-uIU70htIbLaGy_Gb8&callback=initMap"></script>
 
