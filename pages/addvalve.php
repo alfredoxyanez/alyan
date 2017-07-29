c6911ec2e291883e68e76c9d67027cc3a289cc02
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-07-29 01:23:24 -0400

    generic

--- pages/addvalve.php
+++ pages/addvalve.php
@@ -6,7 +6,9 @@ $parkdbname = mysqli_real_escape_string($mysqli,$_POST['parkdbname']);
 $valveid = mysqli_real_escape_string($mysqli,$_POST['valveid']);
 $valveid= preg_replace('/\s+/', '', $valveid);
 $numtrees = mysqli_real_escape_string($mysqli,$_POST['trees']);
+$numtrees = intval($numtrees);
 $numgals = mysqli_real_escape_string($mysqli,$_POST['gals']);
+$numgals =floatval($numgals);
 
 if(IDisused($valveid,$parkdbname)){
   die(header("HTTP/1.0 404 Not Found"));
