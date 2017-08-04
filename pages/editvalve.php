85aa6bc591a78a6be52d4b97c503523c6c5533a7
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-08-03 23:26:51 -0400

    generic

--- pages/editvalve.php
+++ pages/editvalve.php
@@ -1,24 +1,24 @@
 <?php
-require "db.php";
-require_once 'json.php';
+require 'json.php';
+require 'db.php';
 
 
-$parkdbname = mysqli_real_escape_string($mysqli,$_POST['parkdbname']);
+// $parkdbname = mysqli_real_escape_string($mysqli,$_POST['parkdbname']);
 
 $valveid = mysqli_real_escape_string($mysqli,$_POST['valveid']);
 $valveid= preg_replace('/\s+/', '', $valveid);
 
-$newid = mysqli_real_escape_string($mysqli,$_POST['newid']);
-$newid= preg_replace('/\s+/', '', $newid);
+// $newid = mysqli_real_escape_string($mysqli,$_POST['newid']);
+// $newid= preg_replace('/\s+/', '', $newid);
+//
+// $numtrees = mysqli_real_escape_string($mysqli,$_POST['trees']);
+// $numtrees = intval($numtrees);
+//
+// $numgals = mysqli_real_escape_string($mysqli,$_POST['gals']);
+// $numgals = split(' ',$numgals);
+// $numgals =floatval($numgals[0]).' '.$numgals[1];
 
-$numtrees = mysqli_real_escape_string($mysqli,$_POST['trees']);
-$numtrees = intval($numtrees);
-
-$numgals = mysqli_real_escape_string($mysqli,$_POST['gals']);
-$numgals = split(' ',$numgals);
-$numgals =floatval($numgals[0]).' '.$numgals[1];
-
-echo $valveid.$parkdbname.$newid.$numtrees.$numgals;
+echo $valveid;
 // if(IDisused($newid,$parkdbname)){
 //   die(header("HTTP/1.0 404 Not Found"));
 // }else{
