1fdd881d591b09a421db34eedbdf9259b410c0cb
ayanez17 <alfredoyanez@Alfredos-Device.local>
2017-08-03 23:40:03 -0400

    generic

--- pages/editvalve.php
+++ pages/editvalve.php
@@ -14,10 +14,12 @@ $newid= preg_replace('/\s+/', '', $newid);
 $numtrees = mysqli_real_escape_string($mysqli,$_POST['trees']);
 $numtrees = intval($numtrees);
 //
-$numgals = (string) mysqli_real_escape_string($mysqli,$_POST['gals']);
+$numgals = mysqli_real_escape_string($mysqli,$_POST['gals']);
+echo($numgals);
 $numgals = split(' ',$numgals);
 // $numgals =floatval($numgals[0]).' '.$numgals[1];
-print_r( $numgals);
+echo($numgals);
+
 //echo $valveid.$parkdbname.$newid.$numtrees.$numgals;
 // if(IDisused($newid,$parkdbname)){
 //   die(header("HTTP/1.0 404 Not Found"));
